a81c092a6f1f07d53561a0d1c936a34b82601b90
DarkMerlin <DarkMerlin@LAPTOP-NKGS89P9>
2023-08-27 04:08:18 +0700

    Test git push

---
+++ backend/models/product.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    isWireless: {
+        type: Boolean,
+        required: true
+    },
+    isRGB: {
+        type: Boolean,
+        required: true
+    },
+    imgPath: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+})
