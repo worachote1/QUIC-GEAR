d9b7d21ab44ad5da3dffca9fe7cdcaad3e6d9554
lattapong <lattapong.iq@gmail.com>
2023-08-27 19:32:39 +0700

    Nav+Side

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Sidebar from ".././components/Sidebar";
 
 export default function Home() {
   return (
     <div>
+      <div className='flex'>
+      <Sidebar />
         Home
+    </div>
     </div>
   )
 }
