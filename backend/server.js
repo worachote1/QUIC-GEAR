166b68118f5803420cae20e7a743e85af7331aa0
DarkMerlin <DarkMerlin@LAPTOP-NKGS89P9>
2023-08-29 23:40:28 +0700

    merge & conflict backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const connectData = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const connectDb = require('./config/dbConnection');
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/products', require('./routes/product'));
+app.use('/api/users', require('./routes/user'));
+app.use('/api/orders', require('./routes/order'));
+app.use("/api/auctionProducts", require('./routes/auctionProductsRoutes'));
+app.use("/api/transactions", require('./routes/transactionsRoutes'));
 
 app.listen(process.env.PORT, () => {
     console.log(`server's running on port ${process.env.PORT}`);
