c7ee785695126f4b0424030d0004611734965530
DarkMerlin <DarkMerlin@LAPTOP-NKGS89P9>
2023-08-30 00:11:36 +0700

    Update product model

--- backend/models/product.js
+++ backend/models/product.js
@@ -42,6 +42,14 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    totalOrder: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('product', productSchema);
