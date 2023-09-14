0542fbda511a22171a89f981254665c37f028f23
DarkMerlin <DarkMerlin@LAPTOP-NKGS89P9>
2023-09-14 16:29:47 +0700

    update productView

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Navbar = () => {
     return (
         <nav className="flex bg-[#ffffff] border-2 border-[#ffffff] border-b-[#e5e5e5] justify-between items-center h-16 px-4 mx-auto text-white">
