5f41938bd08f63472b24bcac6b078ecc69544b80
worachote1 <timeworachote2002@gmail.com>
2023-10-06 16:27:52 +0700

    fix navBar session

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Route, Link, useNavigate } from 'react-router-dom';
 import { formatNumberInput } from "../util/formatUtil";
+import axios from "axios";
 
 export default function Navbar() {
-    const current_user = JSON.parse(sessionStorage.getItem('current_user'));
+
+    const current_user = JSON.parse(sessionStorage.getItem('current_user'))
+    const [updateUser, setUpdateUser] = useState(current_user)
     const [profileMenuActive, setProfileMenuActive] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
     const navigate = useNavigate();
@@ -11,13 +14,28 @@ export default function Navbar() {
     const clickProfileDropdown = () => {
         setProfileMenuActive(!profileMenuActive);
     };
-    
+
     const logOut = () => {
         clickProfileDropdown()
         sessionStorage.removeItem('current_user');
+        setUpdateUser(null)
     }
 
+    const getSingleUser = async () => {
+        if (!current_user){
+            return ;
+        }
+        const res_getUserData = await axios.get(`${process.env.REACT_APP_QUIC_GEAR_API}/users/${current_user._id}`);
+        setUpdateUser({ ...res_getUserData.data });
+        sessionStorage.setItem('current_user', JSON.stringify(res_getUserData.data))
+    }
+
+    useEffect(() => {
+        getSingleUser()
+    }, [])
+
     return (
+
         <div className="sticky top-0 z-40 bg-white">
             <nav className="flex flex-col md:flex-row items-center p-4 border-b-2 border-[#dbdbdb] shadow-md">
                 <div className="w-full md:w-1/2 text-center md:text-left">
@@ -150,5 +168,7 @@ export default function Navbar() {
                 </div>
             </nav>
         </div>
+
+
     );
 }
