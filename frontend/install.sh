fc30376fc46acff7a5fb6b27b8dc30f34957ff48
Late Night Defender <pongpeera054@protonmail.com>
2023-10-12 20:44:07 +0700

    Run frontend in dev mode (for now)

--- frontend/install.sh
+++ frontend/install.sh
@@ -2,7 +2,7 @@ podman stop quicgear
 podman rm quicgear
 
 podman build -t quicgear .
-podman run --name quicgear -p 8083:80 -d quicgear
+podman run --name quicgear -p 8083:80 -v ./public/uploads:/opt/quicgear/public/uploads -d quicgear
 
 podman generate systemd quicgear > ~/.config/systemd/user/quicgear.service
 systemctl --user daemon-reload
