4b4743c19f348f1833c9042ef02c642dcd95dbbc
Late Night Defender <pongpeera054@protonmail.com>
2023-11-12 03:30:16 +0700

    Upgrade deployment to use Podman Quadlets

--- backend/install.sh
+++ backend/install.sh
@@ -2,8 +2,6 @@ podman stop quicgear-back
 podman rm quicgear-back
 
 podman build -t quicgear-back .
-podman run --name quicgear-back -p 5000:5000 -v ../frontend:/opt/frontend -d quicgear-back
-
-podman generate systemd quicgear-back > ~/.config/systemd/user/quicgear-back.service
+cp quicgear-back.container ~/.config/containers/systemd
 systemctl --user daemon-reload
-systemctl --user enable quicgear-back.service
+systemctl --user start quicgear-back
