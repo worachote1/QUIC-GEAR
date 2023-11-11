81bdb13729a7f9f5c3cc5f29e0fc5a169cff9068
Late Night Defender <pongpeera054@protonmail.com>
2023-11-12 03:39:11 +0700

    Upgrade frontend deployment to quadlet

--- frontend-install.sh
+++ frontend-install.sh
@@ -1,1 +1,9 @@
+systemctl --user stop quicgear
+
+podman build -t quicgear .
+cp frontend/quicgear.container ~/.config/containers/systemd
+systemctl --user daemon-reload
+systemctl --user start quicgear
+
+
 podman run --name quicgear -p 8083:3000 -v ./frontend:/opt/frontend -d quicgear yarn start
